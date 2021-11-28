32f3cbf1221faf1c35a083b268b3759bfbe7eae2
Spr_zero <81459719+nk712001@users.noreply.github.com>
2021-11-28 21:21:31 +0530

    Add files via upload

---
+++ firstNonrep.java
@@ -0,0 +1,21 @@
+package edyoda7;
+import java.util.*;
+import java.util.stream.*;
+import java.util.function.Function;
+public class firstNonrep {
+
+        public static void main(String args[]) {
+            String input = "Java Hungry Blog Alive is Awesome";
+
+            Character result = input.chars() // Stream of String
+                    .mapToObj(s -> Character.toLowerCase(Character.valueOf((char) s)))
+                    .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting())) 
+                    .entrySet()
+                    .stream()
+                    .filter(entry -> entry.getValue() == 1L)
+                    .map(entry -> entry.getKey())
+                    .findFirst()
+                    .get();
+            System.out.println(result);
+        }
+}
